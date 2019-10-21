951742cc4022cb3f1c539587d0c6453fd65dfa84
8aku <churzed@gmail.com>
2019-10-21 14:44:22 -0500

    changed route... fixed html validation errors

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 	resources :providers, only: %i[index]
 	resources :about
 
-	root to: 'birdsongs#index'
+	root to: 'birds#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
